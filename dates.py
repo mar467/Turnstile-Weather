6d3e99697f06d5f709c536160212bea9161c9af7
Moiz Rauf <moizrauf@gmail.com>
2015-11-20 07:35:51 -0500

    Add parent Dates class. Make skeleton

--- dates.py
+++ dates.py
@@ -5,3 +5,19 @@ Created on Fri Nov 20 07:30:39 2015
 @author: moizr_000
 """
 
+class Date(object):
+    def __init__(self, month, day, year):
+        self.date = self.make_date(month, day, year)
+        self.original_date = self.date
+        
+    def make_date(day, month, year): 
+        pass
+    def get_date(): 
+        pass
+    def get_day(): 
+        pass
+    def get_month():
+        pass
+    def get_year():
+        pass
+        
