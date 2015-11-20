6d31eeb8f799b98eb4d7d3118273482b0081b6d7
Moiz Rauf <moizrauf@gmail.com>
2015-11-20 09:38:20 -0500

    Make MTALink child class with skeleton. Plus import date statement

--- links.py
+++ links.py
@@ -5,9 +5,22 @@ Created on Fri Nov 20 09:31:18 2015
 @author: moizr_000
 """
 
+import dates
+
 class Link(object):
     def __init__(self):
         self.url = ""
         
     def get_url(self):
         return self.url
+        
+class MTALink(Link):
+    def __init__(self, subway_date):
+        Link.__init__(self)
+        self.date = subway_date
+        self.make_url()
+        
+    def make_url():
+        pass # will need to use subway_date.get_[day, month, year]()
+    
+        
