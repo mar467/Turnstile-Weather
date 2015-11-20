31b3383a1a3302353a6782856f603216e6c020d9
Moiz Rauf <moizrauf@gmail.com>
2015-11-20 07:52:34 -0500

    Put in "self" arguments into all methods

--- dates.py
+++ dates.py
@@ -7,18 +7,20 @@ Created on Fri Nov 20 07:30:39 2015
 
 class Date(object):
     def __init__(self, month, day, year):
+        # month, day, year not included as attributes because liable to change frequently
+        # instead, will use datetime objects
         self.date = self.make_date(month, day, year)
         self.original_date = self.date
         
     def make_date(day, month, year): 
         pass
-    def get_date(): 
+    def get_date(self): 
         pass
-    def get_day(): 
+    def get_day(self): 
         pass
-    def get_month():
+    def get_month(self):
         pass
-    def get_year():
+    def get_year(self):
         pass
 
 # MTA stands for Metro Transit Authority   
@@ -28,11 +30,11 @@ class MTADate(Date):
         self.make_Saturday()
         
     # MTA API only provides data on a weekly basis, corresponding to Saturday dates   
-    def make_Saturday():
+    def make_Saturday(self):
         pass
-    def make_next_Saturday():
+    def make_next_Saturday(self):
         pass
-    def make_last_Saturday():
+    def make_last_Saturday(self):
         pass
         
 # WU stands for Weather Underground
@@ -41,9 +43,9 @@ class WUDate(Date):
         Date.__init__(self, month, day, year)
        
     # WU API provides data on daily basis   
-    def make_tomorrow():
+    def make_tomorrow(self):
         pass
-    def make_yesterday():
+    def make_yesterday(self):
         pass
     
 class DateList(object):
@@ -52,11 +54,11 @@ class DateList(object):
         self.date_max = date_max
         self.date_list = [] # list of date objects
         
-    def get_min_date():
+    def get_min_date(self):
         pass
-    def get_max_date():
+    def get_max_date(self):
         pass
-    def get_date_list():
+    def get_date_list(self):
         pass
     
     
