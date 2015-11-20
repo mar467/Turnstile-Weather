415c44bd7bfb957fcdfbc63e8776defecfebf62e
Moiz Rauf <moizrauf@gmail.com>
2015-11-20 07:59:16 -0500

    Add MTADateList child class that inherits from DateList with initialization function

--- dates.py
+++ dates.py
@@ -61,4 +61,20 @@ class DateList(object):
     def get_date_list(self):
         pass
     
-    
+### since we will be using a range of dates, it will be useful 
+### to make list of Saturday dates for subsequent indexing when extracting data
+class MTADateList(DateList):
+    def __init__(self, date_min, date_max):
+        DateList.__init__(self, date_min, date_max)
+        mta_date = MTADate(self.date_min) # initializes to Saturday
+        
+        self.date_list.append(mta_date)
+        
+        while mta_date.get_date() <= date_max.get_date():
+            mta_date.make_next_Saturday()
+            self.date_list.append(mta_date)
+        
+        self.num_weeks = len(self.date_list)
+        
+    def get_num_weeks(self):
+        return self.num_weeks
