ac33af6ad7de557d89600e972c42537f00a6de76
Moiz Rauf <moizrauf@gmail.com>
2015-11-20 09:34:49 -0500

    Make Links parent class

--- links.py
+++ links.py
@@ -5,3 +5,9 @@ Created on Fri Nov 20 09:31:18 2015
 @author: moizr_000
 """
 
+class Link(object):
+    def __init__(self):
+        self.url = ""
+        
+    def get_url(self):
+        return self.url
