ae5bb8022ba03f1d2729726af3ebc5c25ae93a5d
Moiz Rauf <moizrauf@gmail.com>
2015-11-21 10:47:00 -0500

    Make links.py classes follow Easy naming terminology as well, for consistency

--- links.py
+++ links.py
@@ -5,15 +5,13 @@ Created on Fri Nov 20 09:31:18 2015
 @author: moizr_000
 """
 
-import dates
-
-class Link(object):
+class EasyLink(object):
     def __init__(self):
         self.url = ""
         
-class MTALink(Link):
+class MTAEasyLink(EasyLink):
     def __init__(self, mta_ezdate):
-        Link.__init__(self)
+        EasyLink.__init__(self)
         self.ezdate = mta_ezdate
         self.make_url()
         
@@ -23,9 +21,9 @@ class MTALink(Link):
         year = self.ezdate.get_abbrev_year_str()
         self.url = "http://web.mta.info/developers/data/nyct/turnstile/turnstile_"+year+month+day+".txt"
     
-class WULink(Link):
+class WUEasyLink(EasyLink):
     def __init__(self, wu_ezdate, location="KNYC"):
-        Link.__init__(self)
+        EasyLink.__init__(self)
         self.ezdate = wu_ezdate
         self.location = location
         self.make_url()
@@ -36,29 +34,29 @@ class WULink(Link):
         year = self.ezdate.get_year_str()
         self.url = "http://www.wunderground.com/history/airport/"+self.location+"/"+year+"/"+month+"/"+day+"/DailyHistory.html?req_city=New+York&req_state=NY&req_statename=New+York&reqdb.zip=10002&reqdb.magic=5&reqdb.wmo=99999&MR=1&format=1"
 
-class LinkList(object): # NOT a link"ed" list, just a list of URL strings
+class EasyLinkList(object): # NOT a link"ed" list, just a list of URL strings
     def __init__(self):
         self.url_list = []
 
         
-class MTALinkList(LinkList):
+class MTAEasyLinkList(EasyLinkList):
     def __init__(self, mta_ezdate_list):
-        LinkList.__init__(self)
+        EasyLinkList.__init__(self)
         self.list_of_ezdates = mta_ezdate_list.ezdate_list
         self.make_url_list()
         
     def make_url_list(self):
         for ezdate in self.list_of_ezdates:
-            self.url_list.append(MTALink(ezdate))
+            self.url_list.append(MTAEasyLink(ezdate))
         
-class WULinkList(LinkList):
+class WUEasyLinkList(EasyLinkList):
     def __init__(self, wu_ezdate_list, location='KNYC'):
-        LinkList.__init__(self)
+        EasyLinkList.__init__(self)
         self.list_of_ezdates = wu_ezdate_list.ezdate_list
         self.location = location
         self.make_url_list()
         
     def make_url_list(self):
         for ezdate in self.list_of_ezdates:
-            self.url_list.append(WULink(ezdate, self.location))
+            self.url_list.append(WUEasyLink(ezdate, self.location))
     
