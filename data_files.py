520a707e82a4c1de239b24c3d819d0a093c7e4d4
Moiz Rauf <moizrauf@gmail.com>
2015-11-22 10:15:22 -0500

    Make MasterFileWriter parent class

--- data_files.py
+++ data_files.py
@@ -5,3 +5,10 @@ Created on Sun Nov 22 10:06:49 2015
 @author: moizr_000
 """
 
+class MasterFileWriter(object):
+    def __init__(self, filename):
+        self._filename = filename
+        
+    def get_path(self):
+        pass
+    
