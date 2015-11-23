4d2a8f6560cda85da0550de0dbd165705ad88bcc
Moiz Rauf <moizrauf@gmail.com>
2015-11-23 11:19:12 -0500

    Make MTADataFrame child class skeleton

--- dataframes.py
+++ dataframes.py
@@ -11,3 +11,23 @@ class DataFrame(object):
     def __init__(self, csv_filepath):
         self.df = pandas.read_csv(csv_filepath)
         
+class MTADataFrame(DataFrame):
+    def __init__(self, csv_filepath):
+        DataFrame.__init__(self, csv_filepath)
+        self._make_datetime_col()
+        self._make_hourly_entries_col()
+        self._make_hourly_exits_col()
+        self._clean_up()
+        
+    def _make_datetime_col(self):
+        pass
+    
+    def _make_hourly_entries_col(self):
+        pass
+    
+    def _make_hourly_exits_col(self):
+        pass
+    
+    def _clean_up(self):
+        pass
+    
