021ea826e5d98461117cb5e6c611e51854a0a473
Moiz Rauf <moizrauf@gmail.com>
2015-11-28 07:21:29 -0500

    Create statistics module and parent class

---
+++ statistics.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 28 07:14:08 2015
+
+@author: moizr_000
+"""
+
+class TurnstileWeatherStatistics(object):
+    def __init__(self, turnstile_weather_df):
+        self.df = turnstile_weather_df
+        
