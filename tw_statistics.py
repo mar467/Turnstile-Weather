fa70db2f49ab4c1977184737603e4682bcc72f9a
Moiz Rauf <moizrauf@gmail.com>
2015-11-29 14:33:12 -0500

    Made code a LOT cleaner by making separate conditions selection method

--- tw_statistics.py
+++ tw_statistics.py
@@ -25,55 +25,7 @@ class Analyzer(WrangledDataFrame):
     def __init__(self, turnstile_weather_df):
         WrangledDataFrame.__init__(self, turnstile_weather_df)
         
-    def entries_histogram(self, selection='rain', skies='Clear'):
-        ''' 
-        skies:
-            ['Clear', 'Mostly Cloudy', 'Overcast', 'Partly Cloudy',
-             'Scattered Clouds', 'Light Rain', 'Haze', 'Rain', 'Light Snow',
-             'Heavy Rain', 'Snow', 'Light Freezing Rain', 'Unknown',
-             'Heavy Snow', 'Mist']
-        '''
-        
-        if selection == 'rain':
-            plt.figure()
-            self.df[self.df["Events"]!='Rain']['Entries Per Hour'].hist()
-            self.df[self.df["Events"]=="Rain"]['Entries Per Hour'].hist()
-            return plt
-        if selection == 'skies':
-            plt.figure()
-            self.df[self.df["Conditions"]==skies]['Entries Per Hour'].hist()
-            self.df[self.df["Conditions"]!=skies]['Entries Per Hour'].hist()
-            return plt
-        if selection == 'visibility':
-            plt.figure()
-            self.df[self.df["VisibilityMPH"]==10]['Entries Per Hour'].hist()
-            self.df[self.df["VisibilityMPH"]!=10]['Entries Per Hour'].hist()
-            return plt
-        '''
-        if selection == 'gusts':
-            plt.figure()
-            self.df[self.df["Gust SpeedMPH"]=='-']['Entries Per Hour'].hist() # FIX AFTER WRANGLED
-            self.df[self.df["Gust SpeedMPH"]!='-']['Entries Per Hour'].hist() # FIX AFTER WRANGLED
-            return plt
-        '''
-        if selection == 'wind direction':
-            plt.figure()
-            self.df[self.df["WindDirDegrees"]==0]['Entries Per Hour'].hist()
-            self.df[self.df["WindDirDegrees"]!=0]['Entries Per Hour'].hist()
-            return plt
-        if selection == 'humidity':
-            plt.figure()
-            self.df[self.df["Humidity"]>56]['Entries Per Hour'].hist()
-            self.df[self.df["Humidity"]<56]['Entries Per Hour'].hist()
-            return plt
-        if selection == 'temperature':
-            plt.figure()
-            self.df[self.df["TemperatureF"]>50]['Entries Per Hour'].hist()
-            self.df[self.df["TemperatureF"]<50]['Entries Per Hour'].hist()
-            return plt
-        print "Not a valid selection."
-        
-    def mann_whitney_plus_means(self, selection='rain', skies='Clear'):
+    def _condition(self, selection, skies):
         ''' 
         skies:
             ['Clear', 'Mostly Cloudy', 'Overcast', 'Partly Cloudy',
@@ -83,88 +35,68 @@ class Analyzer(WrangledDataFrame):
         '''
              
         if selection == 'rain':
-            with_rain = self.df[self.df["Events"]=='Rain']['Entries Per Hour'] # series
-            without_rain = self.df[self.df["Events"]!='Rain']['Entries Per Hour']
-            with_rain_mean = np.mean(with_rain)
-            without_rain_mean = np.mean(without_rain)
+            with_cond = self.df["Events"]=='Rain'
+            without_cond = self.df["Events"]!='Rain'
             
-            with_rain_size = with_rain.size
-            without_rain_size = without_rain.size
+        elif selection == 'skies': # needs to catch potential errors with skies
+            with_cond = self.df["Conditions"]==skies
+            without_cond = self.df["Conditions"]!=skies
             
-            U, p = scipy.stats.mannwhitneyu(with_rain, without_rain, use_continuity=True)
-            return (with_rain_size, with_rain_mean), (without_rain_size, without_rain_mean), U, p
+        elif selection == 'visibility':
+            with_cond = self.df["VisibilityMPH"]==10
+            without_cond = self.df["VisibilityMPH"]<10
+
+        elif selection == 'gusts':
+            with_cond = self.df["Gust SpeedMPH"]=='-' # FIX AFTER WRANGLED
+            without_cond = self.df["Gust SpeedMPH"]!='-' # FIX AFTER WRANGLED
             
-        if selection == 'skies':
-            cond = self.df[self.df["Conditions"]==skies]['Entries Per Hour'] # series
-            not_cond = self.df[self.df["Conditions"]!=skies]['Entries Per Hour']
-            cond_mean = cond.mean()
-            not_cond_mean = not_cond.mean()
+        elif selection == 'wind direction':
+            with_cond = self.df["WindDirDegrees"]==0
+            without_cond = self.df["WindDirDegrees"]!=0
+      
+        elif selection == 'precipitation':
+            with_cond = self.df["PrecipitationIn"]>0.01
+            without_cond = self.df[np.isnan(self.df["PrecipitationIn"])] # FIX AFTER WRANGLED
             
-            cond_size = cond.size
-            not_cond_size = not_cond.size
+        elif selection == 'temperature':
+            with_cond = self.df["TemperatureF"]>50
+            without_cond = self.df["TemperatureF"]<50
             
-            U, p = scipy.stats.mannwhitneyu(cond, not_cond, use_continuity=True)
-            return (cond_size, cond_mean), (not_cond_size, not_cond_mean), U, p
+        elif selection == 'humidity':
+            with_cond = self.df["Humidity"]>56
+            without_cond = self.df["Humidity"]<56
+  
+        elif selection == 'pressure':
+            with_cond = self.df["Sea Level PressureIn"]>33
+            without_cond = self.df["Sea Level PressureIn"]<33
             
-        if selection == 'visibility':
-            high_visibility = self.df[self.df["VisibilityMPH"]==10]['Entries Per Hour'] # series
-            low_visibility = self.df[self.df["VisibilityMPH"]<2]['Entries Per Hour']
-            high_visibility_mean = np.mean(high_visibility)
-            low_visibility_mean = np.mean(low_visibility)
+        else:
+            print 'Not a valid selection.'
+            with_cond = self.df["Events"]=='Rain'
+            without_cond = self.df["Events"]!='Rain'
             
-            high_visibility_size = high_visibility.size
-            low_visibility_size = low_visibility.size
+        return with_cond, without_cond
             
-            U, p = scipy.stats.mannwhitneyu(high_visibility, low_visibility, use_continuity=True)
-            return (high_visibility_size, high_visibility_mean), (low_visibility_size, low_visibility_mean), U, p
         
-        '''
-        if selection == 'gusts':
-            with_gusts = self.df[self.df["Gust SpeedMPH"]=='-']['Entries Per Hour'] # FIX AFTER WRANGLED
-            without_gusts = self.df[self.df["Gust SpeedMPH"]!='-']['Entries Per Hour'] # FIX AFTER WRANGLED
-            with_gusts_mean = np.mean(with_gusts)
-            without_gusts_mean = np.mean(without_gusts)
-            
-            with_gusts_size = with_gusts.size
-            without_gusts_size = without_gusts.size
-            
-            U, p = scipy.stats.mannwhitneyu(with_gusts, without_gusts, use_continuity=True)
-            return (with_gusts_size, with_gusts_mean), (without_gusts_size, without_gusts_mean), U, p
-        '''
-            
-        if selection == 'wind direction':
-            zero_deg = self.df[self.df["WindDirDegrees"]==0]['Entries Per Hour']
-            not_zero_deg = self.df[self.df["WindDirDegrees"]!=0]['Entries Per Hour']
-            zero_deg_mean = np.mean(zero_deg)
-            not_zero_deg_mean = np.mean(not_zero_deg)
-            
-            zero_deg_size = zero_deg.size
-            not_zero_deg_size = not_zero_deg.size
-            
-            U, p = scipy.stats.mannwhitneyu(zero_deg, not_zero_deg, use_continuity=True)
-            return (zero_deg_size, zero_deg_mean), (not_zero_deg_size, not_zero_deg_mean), U, p
-            
-        if selection == 'precipitation':
-            precip = self.df[self.df["PrecipitationIn"]>0.01]['Entries Per Hour']
-            no_precip = self.df[np.isnan(self.df["PrecipitationIn"])]['Entries Per Hour']
-            precip_mean = np.mean(precip)
-            no_precip_mean = np.mean(no_precip)
-            
-            precip_size = precip.size
-            no_precip_size = no_precip.size
-            
-            U, p = scipy.stats.mannwhitneyu(precip, no_precip, use_continuity=True)
-            return (precip_size, precip_mean), (no_precip_size, no_precip_mean), U, p
-            
-            
-        if selection == 'temperature':
-            hot = self.df[self.df["TemperatureF"]>56]['Entries Per Hour']
-            cold = self.df[self.df["TemperatureF"]<56]['Entries Per Hour']
-            hot_mean = np.mean(hot)
-            cold_mean = np.mean(cold)
-            
-            hot_size = hot.size
-            cold_size = cold.size
-            
-            U, p = scipy.stats.mannwhitneyu(hot, cold, use_continuity=True)
-            return (hot_size, hot_mean), (cold_size, cold_mean), U, p
+    def entries_histogram(self, selection='rain', skies='Clear', opposite=False):
+        if opposite:
+            without_cond, with_cond = self._condition(selection, skies)
+        else:
+            with_cond, without_cond = self._condition(selection, skies)
+        
+        plt.figure()
+        self.df[with_cond]['Entries Per Hour'].hist()
+        self.df[without_cond]['Entries Per Hour'].hist()
+        return plt
+        
+    def mann_whitney_plus_means(self, selection='rain', skies='Clear'):
+        with_cond, without_cond = self._condition(selection, skies)
+        
+        cond = self.df[with_cond]['Entries Per Hour']
+        no_cond = self.df[without_cond]['Entries Per Hour']
+        cond_mean = np.mean(cond)
+        no_cond_mean = np.mean(no_cond)
+        
+        U, p = scipy.stats.mannwhitneyu(cond, no_cond, use_continuity=True)
+        return (cond.size, cond_mean), (cond.size, no_cond_mean), U, p
+        
